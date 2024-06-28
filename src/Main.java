a3bdddfd0d985313f9fdeafafa53ea8f80a4173f
NordCoder <dimakorzh2005@gmail.com>
2024-06-28 15:55:47 +0300

    git test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import shell.Shell;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to shell shell shell shell shell shell shell");
         Shell shell = new Shell();
     }
 }
